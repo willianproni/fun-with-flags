cc7e8f3955052c80725ec2dea9d8164214bd91b8
willianproni <pronifwillian@gmail.com>
2025-05-29 22:51:48 -0300

    refactor - add new url images:{remotePatterns} in Next Config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://placehold.co/**')],
+    remotePatterns: [new URL('https://flagcdn.com/w320/**'), new URL('https://upload.wikimedia.org/wikipedia/commons/**')],
   },
 };
 
