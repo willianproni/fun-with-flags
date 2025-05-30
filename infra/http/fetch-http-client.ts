9b6f268cf0ba6a595d75d5ae7afd518f0f03f8c3
willianproni <pronifwillian@gmail.com>
2025-05-29 21:10:37 -0300

    Feature - Create featch http client with abstraction http client

---
+++ infra/http/fetch-http-client.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { methodHttp } from "@/@shared/protocols/http/httpMethod";
+import { HttpClient } from "@/data/protocols/http/http-client";
+import { HttpResponse } from "@/data/protocols/http/http-response";
+
+export class FetchHttpClient implements HttpClient<any, any> {
+  async request(params: {
+    url: string;
+    method: methodHttp;
+    body?: any;
+  }): Promise<HttpResponse<any>> {
+    try {
+      const { method, url } = params;
+
+      const response = await fetch(url, { method, body: params?.body });
+
+      if (!response.ok) {
+        const errorData = await response.json().catch(() => ({}));
+        throw new Error(errorData.message || `Erro HTTP: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      return {
+        statusCode: response.status,
+        body: data,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+}
