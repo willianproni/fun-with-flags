98251e7d4d9fa2fafda9e1280a0ce8e509b2c499
willianproni <pronifwillian@gmail.com>
2025-06-01 14:55:45 -0300

    refactor -request Countries by Server Side

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,51 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import { Card } from "./components";
-import { flagProps } from "./components/Card";
 import { FetchHttpClient } from "@/infra/http/fetch-http-client";
 import { RemoteFlagsSearch } from "@/data/usecases/remote-flags-search";
+import { flagProps } from "@/domain/model/flags-model";
 
-export default function Home() {
-  const [countriesList, setCountriesList] = useState<flagProps[]>([]);
+const fetchHttpClient = new FetchHttpClient();
 
-  const fetchHttpClient = new FetchHttpClient();
+const searchFlagsUseCase = new RemoteFlagsSearch(fetchHttpClient);
 
-  const seacrhFlagsUseCase = new RemoteFlagsSearch(fetchHttpClient);
+const fetchCountries = async () => {
+  try {
+    const response = await searchFlagsUseCase.execute();
 
-  useEffect(() => {
-    const fetchCountries = async () => {
-      const response = await seacrhFlagsUseCase.execute();
+    return response;
+  } catch (error) {
+    console.log({ error });
+    return [];
+  }
+};
 
-      setCountriesList(response);
-    };
-    fetchCountries();
-  }, []);
+export default async function Home() {
+  const flagsInfo: flagProps[] = await fetchCountries();
 
   return (
-    <>
-      <main className="flex-1">
+    <main className="flex-1">
+      {flagsInfo.length ? (
         <div className="grid md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-6">
-          {countriesList.map((item, index) => {
-            return (
-              <Card
-                key={index}
-                image={item.image}
-                country={item.country}
-                capital={item.capital}
-                region={item.region}
-                population={item.population}
-              />
-            );
-          })}
+          {flagsInfo?.map(
+            ({ image, capital, country, population, region, id }) => {
+              return (
+                <Card
+                  key={id}
+                  id={id}
+                  image={image}
+                  country={country}
+                  capital={capital}
+                  region={region}
+                  population={population}
+                />
+              );
+            }
+          )}
         </div>
-      </main>
-    </>
+      ) : (
+        <div className="flex justify-center">
+          <span className="text-3xl">No flags found</span>
+        </div>
+      )}
+    </main>
   );
 }
