cc9835c167bc3787c30abc9d3dfe7193d4d8a82a
willianproni <pronifwillian@gmail.com>
2025-06-07 18:21:50 -0300

    Fix - Card component

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -18,21 +18,18 @@ export const Card = ({
   population,
 }: flagCardProps) => {
   return (
-    <div
-      className="h-full bg-white overflow-hidden rounded-lg shadow-lg
-     hover:shadow-xl hover:-translate-y-1 transform transition-all duration-300 ease-in-out"
-    >
+    <div className="h-full overflow-hidden bg-white rounded-lg shadow-lg hover:shadow-xl hover:-translate-y-1 transform transition-all duration-300 ease-in-out">
       <div className="aspect-video w-full">
         <Image
           className="w-full h-full object-cover"
-          src={image}
+          src={image || "https://placehold.co/600x400.svg"}
           width={500}
           height={300}
-          alt="Image flag"
+          alt={`Flag of ${country}`}
           priority={id < 12}
         />
       </div>
-      <div className="p-6">
+      <div className="p-6 text-sm text-gray-600">
         <h2 className="text-xl font-semibold mb-4">{country}</h2>
         <div className="space-y-2">
           <div>
